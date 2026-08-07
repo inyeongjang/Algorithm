29174227bae1b2383bd3d80023a7c645344f3bf0
장인영 <135231946+inyeongjang@users.noreply.github.com>
2026-08-08 01:03:44 +0900

    [level 2] Title: 전화번호 목록, Time: 71.40 ms, Memory: 28.9 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,6 +1,10 @@
+# 시간 복잡도 : O(n logn), 공간 복잡도 : O(1)
+
 def solution(phone_book):
     phone_book.sort()
-    for i in range(len(phone_book)):
+
+    for i in range(1, len(phone_book)):
         if phone_book[i].startswith(phone_book[i - 1]):
             return False
-    return True 
+
+    return True
