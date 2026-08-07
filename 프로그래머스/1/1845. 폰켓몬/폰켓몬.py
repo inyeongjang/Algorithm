bf3bc34a49347bdf74e34010832a623d1c15eb39
장인영 <135231946+inyeongjang@users.noreply.github.com>
2026-08-08 00:50:09 +0900

    [level 1] Title: 폰켓몬, Time: 0.64 ms, Memory: 12.3 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,4 +1,2 @@
-def solution(nums):   
-    if len(nums) / 2 < len(set(nums)):
-        return len(nums) / 2
-    return  len(set(nums))
+def solution(nums):
+    return min(len(nums) // 2, len(set(nums)))
