5ab7dd22c9a0ab7f96c996d9d275049ea6dbc929
장인영 <135231946+inyeongjang@users.noreply.github.com>
2026-08-08 00:41:43 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 46.53 ms, Memory: 30.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,3 +1,6 @@
+# 시간 복잡도 : O(n logn), 공간 복잡도 : O(1)
+
+"""
 def solution(participant, completion):    
     participant.sort()
     completion.sort()
@@ -7,3 +10,19 @@ def solution(participant, completion):
             return participant[i]
     
     return participant[-1]
+"""
+
+# 시간 복잡도 : O(n), 공간 복잡도 : O(n)
+
+def solution(participant, completion):    
+    count = {}
+    
+    for name in participant:
+        count[name] = count.get(name, 0) + 1
+    
+    for name in completion:
+        count[name] -= 1 
+    
+    for name in count:
+        if count[name] > 0:
+            return name 
