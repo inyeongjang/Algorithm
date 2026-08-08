0118a6b59605213c4ad2a4a5676b6036a1a35bee
장인영 <135231946+inyeongjang@users.noreply.github.com>
2026-08-08 15:49:08 +0900

    [level 2] Title: 구명보트, Time: 7.39 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,12 +1,20 @@
+# 시간 복잡도 : O(n logn), 공간 복잡도 : O(1O)
+
 def solution(people, limit):
     answer = 0
     top = 0 
     bottom = len(people) - 1 
+    
+    # O(nlogn)
     people.sort(reverse = True)
     
+    # O(n)
     while top <= bottom:
         answer += 1
+        
         if people[top] + people[bottom] <= limit:
             bottom -= 1 
+        
         top += 1 
+    
     return answer 
