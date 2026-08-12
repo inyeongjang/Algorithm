85098065687cf37a4aa37d6801fe0caf38ea5d48
장인영 <135231946+inyeongjang@users.noreply.github.com>
2026-08-13 01:54:14 +0900

    [level 2] Title: 소수 찾기, Time: 3.22 ms, Memory: 11.8 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,24 +1,27 @@
+# 시간 복잡도 : O(n! * √M), 공간 복잡도 : O(n!)
+
 import itertools
 
 def solution(numbers):
     answer = 0
-    numbers_arr = []
     numbers_set = set()
     
-    for i in range(len(numbers)):
-        numbers_arr.append(itertools.permutations(numbers, i + 1))
-    
-    for permutations in numbers_arr:
-        for p in permutations:
+    for i in range(1, len(numbers) + 1):
+        for p in itertools.permutations(numbers, i):
             numbers_set.add(int("".join(p)))
     
     for number in numbers_set:
-        answer += 1 
         if number < 2:
-            answer -= 1
+            continue 
+        
+        is_prime = True 
+        
         for i in range(2, int(number ** 0.5) + 1):
             if number % i == 0:
-                answer -= 1
+                is_prime = False  
                 break 
-                
+        
+        if is_prime:
+            answer += 1 
+            
     return answer
