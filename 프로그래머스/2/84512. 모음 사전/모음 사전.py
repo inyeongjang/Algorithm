c4a91dd0a3e150bbff276ac172112c4206ae7dae
장인영 <135231946+inyeongjang@users.noreply.github.com>
2026-08-13 01:26:19 +0900

    [level 2] Title: 모음 사전, Time: 0.03 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,11 +1,11 @@
+# 시간 복잡도 : O(n), 공간 복잡도 : O(1)
+
 def solution(word):
-    answer = 0 
-    number = {"A" : 1, "E" : 2, "I" : 3, "O" : 4, "U" : 5}
-    
+    answer = 0
+    number = {"A": 1, "E": 2, "I": 3, "O": 4, "U": 5}
+
     for i in range(len(word)):
-        for j in range(1, number[word[i]]):
-            for k in range(5 - i):
-                answer += 5 ** k 
-        answer += 1
-            
+        weight = sum(5 ** k for k in range(5 - i))
+        answer += (number[word[i]] - 1) * weight + 1
+
     return answer
