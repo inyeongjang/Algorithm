76775461da469487520c135ad6500017f5dba963
장인영 <135231946+inyeongjang@users.noreply.github.com>
2026-08-15 00:24:17 +0900

    [level 2] Title: 카펫, Time: 0.06 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,14 +1,17 @@
-def solution(brown, yellow):
-    answer = []
-    # w * h = brown + yellow 
-    # yellow = (h - 2) * (w - 2)
-    # brown = 2 * w + 2 * h - 4
-    # brown + yellow 의 약수 중 조건을 만족하는 것 
+"""
+    시간 복잡도 : O(√N), 공간 복잡도 : O(1)
     
-    for i in range(1, int((brown + yellow) ** 0.5) + 1) :
-        if (brown + yellow) % i == 0:
-            if brown == 2 * (i + (brown + yellow) // i) - 4:
-                answer.append((brown + yellow) // i)
-                answer.append(i)
-                
-    return answer
+    w * h = brown + yellow 
+    yellow = (h - 2) * (w - 2)
+    brown = 2 * w + 2 * h - 4
+"""
+
+def solution(brown, yellow):
+    total = brown + yellow 
+    
+    for h in range(1, int(total ** 0.5) + 1) :
+        if total % h == 0:
+            w = total // h 
+            
+            if brown == 2 * (w + h) - 4:
+                return [w, h]
