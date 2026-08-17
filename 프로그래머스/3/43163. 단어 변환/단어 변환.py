a5de1cfebdf97806a034ee60700c2b91efee3719
장인영 <135231946+inyeongjang@users.noreply.github.com>
2026-08-17 23:11:27 +0900

    [level 3] Title: 단어 변환, Time: 0.19 ms, Memory: 11.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -0,0 +1,30 @@
+from collections import deque
+
+def solution(begin, target, words):
+    
+    def can_change(a, b):
+        count = 0
+        
+        for i in range(len(a)):
+            if a[i] != b[i]:
+                count += 1
+        
+        return count == 1
+    
+    queue = deque()
+    queue.append((begin, 0))
+    
+    visited = [False] * len(words)
+    
+    while queue:
+        current, steps = queue.popleft()
+        
+        if current == target:
+            return steps
+        
+        for i in range(len(words)):
+            if not visited[i] and can_change(current, words[i]):
+                visited[i] = True
+                queue.append((words[i], steps + 1))
+    
+    return 0
