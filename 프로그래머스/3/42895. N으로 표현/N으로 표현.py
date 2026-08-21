92497b57a0cf0bc11469f2afb874573257b005b5
장인영 <135231946+inyeongjang@users.noreply.github.com>
2026-08-21 23:41:26 +0900

    [level 3] Title: N으로 표현, Time: 7.07 ms, Memory: 12.7 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.py
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.py
@@ -1,16 +1,16 @@
 def solution(N, number):
     dp = [set() for _ in range(9)]
-    dp[1].add(N)
 
     for i in range(1, 9):
         dp[i].add(int(str(N) * i))
 
-        for k in range(1, i):
-            for a in dp[i - k]:
-                for b in dp[k]:
+        for j in range(1, i):
+            for a in dp[j]:
+                for b in dp[i - j]:
                     dp[i].add(a + b)
                     dp[i].add(a - b)
                     dp[i].add(a * b)
+
                     if b != 0:
                         dp[i].add(a // b)
 
